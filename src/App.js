776a0a623f0eeeae41389e4fce4b0c90ea6150b2
Danny Lee <dreamer@panix.com>
2019-11-22 15:29:18 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     );
   }
 
+
   // returns array of components written in JSX, mapped from this.state.timerIDs
   renderTimers = () => this.state.timerIDs.map(({id, updateInterval}) => <Timer key={id} id={id} removeTimer={this.removeTimer} updateInterval={updateInterval}/>)
 
