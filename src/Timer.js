6d8ee4c459879587c79b9942d04f21300c1c3448
Jonathan Tan <you@example.com>
2019-11-21 22:33:22 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-
-class Timer extends Component {
+import React, { PureComponent } from 'react';
+class Timer extends PureComponent {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -10,13 +9,23 @@ class Timer extends Component {
     };
   }
 
-  //Your code here
+ 
 
+  componentDidUpdate() {
+    this.timer.current.style.color ="#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
   componentDidMount() {
     this.interval = setInterval(
       this.clockTick,
       this.props.updateInterval * 1000
     );
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
   }
 
   componentWillUnmount() {
